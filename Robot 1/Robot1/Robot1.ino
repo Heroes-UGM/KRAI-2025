f00f7ad7d223c541c3c3cde62ae951c9fef7c7a5
indahnurrohmah <indahnurrohmah@mail.ugm.ac.id>
2025-07-03 02:40:55 +0700

    feat: change pcb pins

--- Robot 1/Robot1/Robot1.ino
+++ Robot 1/Robot1/Robot1.ino
@@ -1,42 +1,57 @@
 #include <PS4Controller.h>
 
-// Wheel Motor Pins
-#define MOTOR1_A 32
-#define MOTOR1_B 33
-#define MOTOR2_A 25
-#define MOTOR2_B 26
-#define MOTOR3_A 14
-#define MOTOR3_B 27
-#define MOTOR4_A 12 // Linear Actuator for Shooting
-#define MOTOR4_B 13
+// Movement motors
+#define MOTOR3_A 33 //32
+#define MOTOR3_B 32 //33
+#define MOTOR2_A 26 //25
+#define MOTOR2_B 25 //26
+#define MOTOR1_A 14
+#define MOTOR1_B 27
 
-// Shooter Pins
-#define SHOOTER_A 16
-#define SHOOTER_B 17
+// Lifter (Linear Actuator)
+#define LIFTER_A 5 //4
+#define LIFTER_B 17 //16
 
-// Conveyor Pins
-#define R_CONV_A 4
-#define R_CONV_B 5
-#define L_CONV_A 18
-#define L_CONV_B 19
+// Conveyor (shared)
+#define CONVEYOR_A 13 //17
+#define CONVEYOR_B 12 //5
+
+// Shooter
+#define SHOOTER_A 4 //12
+#define SHOOTER_B 16 //13
+
+// // Lifter (Linear Actuator)
+// #define LIFTER_A 12
+// #define LIFTER_B 13
+
+// // Conveyor (shared)
+// #define CONVEYOR_A 4
+// #define CONVEYOR_B 16
+
+// // Shooter
+// #define SHOOTER_A 17
+// #define SHOOTER_B 5
 
 float Vx = 0.0, Vy = 0.0, w = 0.0;
-float v1 = 0.0, v2 = 0.0, v3 = 0.0, v4 = 0.0;
+float v1 = 0.0, v2 = 0.0, v3 = 0.0, lifter = 0.0;
 float shooterSpeed = 0.0;
-float conveyorLeftSpeed = 0.0;
-float conveyorRightSpeed = 0.0;
+float conveyorSpeed = 0.0;
 int mode = 0;
 bool lastTriangleState = false;
+bool conveyorUpActive = false;
+bool conveyorDownActive = false;
+unsigned long conveyorTimer = 0;
+bool lastL1State = false;
+bool lastL2State = false;
+
 
 void setup() {
   Serial.begin(115200);
 
-  // Set all motor pins as OUTPUT
   int motorPins[] = {
     MOTOR1_A, MOTOR1_B, MOTOR2_A, MOTOR2_B,
-    MOTOR3_A, MOTOR3_B, MOTOR4_A, MOTOR4_B,
-    SHOOTER_A, SHOOTER_B, R_CONV_A, R_CONV_B,
-    L_CONV_A, L_CONV_B
+    MOTOR3_A, MOTOR3_B, LIFTER_A, LIFTER_B,
+    SHOOTER_A, SHOOTER_B, CONVEYOR_A, CONVEYOR_B
   };
 
   for (int i = 0; i < sizeof(motorPins) / sizeof(motorPins[0]); i++) {
@@ -49,91 +64,119 @@ void setup() {
 
 void loop() {
   bool currentTriangleState = PS4.Triangle();
+  bool currentL1State = PS4.L1();
+  bool currentL2State = PS4.L2();
+  unsigned long currentTime = millis();
+
   if (PS4.isConnected()) {
-
-    
-  // debouncing Detect rising edge: when button is pressed now but wasn't before
-    if(currentTriangleState && !lastTriangleState){
-      mode = (mode == 0) ? 1 : 0; 
+    if (currentTriangleState && !lastTriangleState) {
+      mode = (mode == 0) ? 1 : 0;
     }
-
-    // Save current state for next loop iteration
     lastTriangleState = currentTriangleState;
 
-    // Map joystick values to motor speeds
-    if(mode == 0){
+    // Movement control
+    if (mode == 0) {
       Vx = map(PS4.LStickX(), -128, 127, -255, 255) / 2;
       Vy = map(PS4.LStickY(), -128, 127, 255, -255) / 2;
-      w = map(PS4.RStickX(), -128, 127, -180, 180) / 2;
-    } else if (mode == 1) {
-      Vx = map(PS4.LStickX(), -128, 127, -255, 255) / 1.5;
-      Vy = map(PS4.LStickY(), -128, 127, 255, -255) / 1.5;
-      w = map(PS4.RStickX(), -128, 127, -180, 180) / 1.5;
+      w  = map(PS4.RStickX(), -128, 127, -180, 180) / 2;
+    } else {
+      Vx = map(PS4.LStickX(), -128, 127, -255, 255) / 1.0;
+      Vy = map(PS4.LStickY(), -128, 127, 255, -255) / 1.0;
+      w  = map(PS4.RStickX(), -128, 127, -180, 180) / 1.0;
     }
-    // Calculate wheel velocities for omni movement
+
     v1 = Vx + w;
     v2 = -Vx * cos(60 * PI / 180) - Vy * cos(30 * PI / 180) + w;
     v3 = -Vx * cos(60 * PI / 180) + Vy * cos(30 * PI / 180) + w;
 
-    // Linear actuator (MOTOR4)
+    // Lifter
     if (PS4.Down()) {
-      v4 = 255;
+      lifter = 255;
     } else if (PS4.Up()) {
-      v4 = -255;
+      lifter = -255;
     } else {
-      v4 = 0;
+      lifter = 0;
     }
 
-    // Shooter control
+    // Shooter
     if (PS4.R2Value() > 0) {
       shooterSpeed = map(PS4.R2Value(), 0, 255, 0, 125);
     } else if (PS4.R1()) {
       shooterSpeed = 80;
+      // shooterSpeed = 80;
     } else {
       shooterSpeed = 0;
     }
 
-    // Conveyor control
+        // Conveyor control
     if (PS4.L2()) {
       // Forward: left slightly slower
-      conveyorLeftSpeed = 225;
-      conveyorRightSpeed = 255;
+      conveyorSpeed = 255;
     } else if (PS4.L1()) {
       // Backward: right slightly slower
-      conveyorLeftSpeed = -225;
-      conveyorRightSpeed = -255;
+      conveyorSpeed = -255;
     } else {
-      conveyorLeftSpeed = 0;
-      conveyorRightSpeed = 0;
+      conveyorSpeed = 0;
     }
+    // // Start conveyor UP sequence (L1 press)
+    // if (currentL1State && !lastL1State && !conveyorUpActive && !conveyorDownActive) {
+    //   conveyorUpActive = true;
+    //   conveyorTimer = currentTime;
+    // }
 
-    // Debug Info
-    Serial.printf("Vx: %.2f\tVy: %.2f\tw: %.2f  \t|| V1: %.2f\tV2: %.2f\tV3: %.2f\tV4: %.2f\tShooter: %.2f\tConv L: %.2f\tConv R: %.2f\tMode: %d\n",
-                  Vx, Vy, w, v1, v2, v3, v4, shooterSpeed, conveyorLeftSpeed, conveyorRightSpeed, mode);
+    // // Start conveyor DOWN sequence (L2 press)
+    // if (currentL2State && !lastL2State && !conveyorDownActive && !conveyorUpActive) {
+    //   conveyorDownActive = true;
+    //   conveyorTimer = currentTime;
+    // }
+
+    // // Conveyor UP logic
+    // if (conveyorUpActive) {
+    //   if (currentTime - conveyorTimer < 420) {
+    //     conveyorSpeed = 255;
+    // } else {
+    //   conveyorSpeed = 0;
+    //   conveyorUpActive = false;
+    // }
+    // }
+
+    // // Conveyor DOWN logic
+    // else if (conveyorDownActive) {
+    //   if (currentTime - conveyorTimer < 310) {
+    //     conveyorSpeed = -255;
+    //   } else {
+    //     conveyorSpeed = 0;
+    //     conveyorDownActive = false;
+    //   }
+    // }
+
+    // // If no active timed movement
+    // else {
+    //   conveyorSpeed = 0;
+    // }
+
+    // // Save button states for rising edge detection 
+    // lastL1State = currentL1State;
+    // lastL2State = currentL2State;
+
+    Serial.printf("Vx: %.2f Vy: %.2f w: %.2f | V1: %.2f V2: %.2f V3: %.2f | Lifter: %.2f | Shooter: %.2f | Conveyor: %.2f | Mode: %d\n",
+      Vx, Vy, w, v1, v2, v3, lifter, shooterSpeed, conveyorSpeed, mode);
     Serial.printf("Battery: %d", PS4.Battery());
     if (PS4.Charging()) Serial.print(" (Charging)");
     Serial.println();
-
   } else {
-    // Controller disconnected: stop all motors
-    Vx = Vy = w = v1 = v2 = v3 = v4 = shooterSpeed = conveyorLeftSpeed = conveyorRightSpeed = 0;
-    Serial.println("CONTROLLER DISCONNECTED\tAll motors set to 0");
+    v1 = v2 = v3 = lifter = shooterSpeed = conveyorSpeed = 0;
+    Serial.println("Controller disconnected");
   }
 
-  // Drive motors
-// Apply deadzone of ±10 for base movement
+  // Apply motors
   controlMotor((abs(v1) > 24) ? v1 : 0, MOTOR1_A, MOTOR1_B);
   controlMotor((abs(v2) > 10) ? v2 : 0, MOTOR2_A, MOTOR2_B);
   controlMotor((abs(v3) > 10) ? v3 : 0, MOTOR3_A, MOTOR3_B);
 
-  controlMotor(v4, MOTOR4_A, MOTOR4_B);
-
-  // Shooter
+  controlMotor(lifter, LIFTER_A, LIFTER_B);
   controlMotor(shooterSpeed, SHOOTER_A, SHOOTER_B);
-
-  // Conveyors
-  controlMotor(conveyorRightSpeed, R_CONV_A, R_CONV_B);
-  controlMotor(conveyorLeftSpeed, L_CONV_A, L_CONV_B);
+  controlMotor(conveyorSpeed, CONVEYOR_A, CONVEYOR_B);
 }
 
 void controlMotor(float v_target, int in1, int in2) {
--- Robot 2/Robot2/Robot2.ino
+++ Robot 2/Robot2/Robot2.ino
@@ -1,15 +1,16 @@
 // ROBOT 2 PNEUMATIC
 #include <PS4Controller.h>
 
-#define MOTOR3_A 12 // Wheel 3
-#define MOTOR3_B 13
-#define MOTOR1_A 32 // Wheel 2
-#define MOTOR1_B 33
+#define MOTOR1_A 27 // Wheel 3
+#define MOTOR1_B 14
+#define MOTOR3_A 32 // Wheel 2
+#define MOTOR3_B 33
 #define MOTOR2_A 25 // Wheel 1
 #define MOTOR2_B 26
-#define MOTOR4_A 14 // Shooter
-#define MOTOR4_B 27
-#define pneumatic 2
+#define MOTOR4_A 12 // Shooter
+#define MOTOR4_B 13
+#define pneumatic_A 4
+#define pneumatic_B 5
 
 float Vx = 0.0;
 float Vy = 0.0;
@@ -18,7 +19,11 @@ float v1 = 0.0;
 float v2 = 0.0;
 float v3 = 0.0;
 float v4 = 0.0;
+float rawVx = 0.0;
+float rawVy = 0.0;
 bool status;
+int mode = 0;
+bool lastRectangleState = false;
 
 bool inverse = false;           // Inverse mode toggle
 bool prevCircleState = false;   // To detect button press transition
@@ -35,10 +40,13 @@ void setup() {
   pinMode(MOTOR3_B, OUTPUT);
   pinMode(MOTOR4_A, OUTPUT);
   pinMode(MOTOR4_B, OUTPUT);
-  pinMode(pneumatic, OUTPUT);
+  pinMode(pneumatic_A, OUTPUT);
+  pinMode(pneumatic_B, OUTPUT);
+
 
   Serial.println("Starting PS4 controller setup...");
-  digitalWrite(pneumatic, LOW);
+  digitalWrite(pneumatic_A, LOW);
+  digitalWrite(pneumatic_B, LOW);
   PS4.begin("20:04:20:05:20:06");
   Serial.println("PS4 controller initialized!");
   Serial.println("Please connect your PS4 controller...");
@@ -46,15 +54,31 @@ void setup() {
 }
 
 void loop() {
+  bool currentRectangleState = PS4.Square();
   if (PS4.isConnected()) {
 
     // Handle inverse mode toggle
     inverseMode();
 
-    // Map joystick values
-    float rawVx = map(PS4.LStickX(), -128, 127, -255, 255) / 3.0;
-    float rawVy = map(PS4.LStickY(), -128, 127, -255, 255) / 3.0;
-    w = map(PS4.RStickX(), -128, 127, -180, 180) / 5.0;
+      // debouncing Detect rising edge: when button is pressed now but wasn't before
+    if(currentRectangleState && !lastRectangleState){
+      mode = (mode == 0) ? 1 : 0; 
+    }
+
+    // Save current state for next loop iteration
+    lastRectangleState = currentRectangleState;
+
+    // Map joystick values to motor speeds
+    if(mode == 0){
+      // Map joystick values
+      rawVx = map(PS4.LStickX(), -128, 127, -255, 255) / 3.0;
+      rawVy = map(PS4.LStickY(), -128, 127, -255, 255) / 3.0;
+      w = map(PS4.RStickX(), -128, 127, -180, 180) / 5.0;
+    } else if (mode == 1) {
+      rawVx = map(PS4.LStickX(), -128, 127, -255, 255) / 2.0;
+      rawVy = map(PS4.LStickY(), -128, 127, -255, 255) / 2.0;
+      w = map(PS4.RStickX(), -128, 127, -180, 180) / 3.0;
+    }
 
     // Apply inverse if active
     Vx = inverse ? -rawVx : rawVx;
@@ -69,23 +93,31 @@ void loop() {
     if (PS4.R2()) {
       v4 = 150;
     } else if (PS4.R1()) {
-      v4 = 100;
+      v4 = 120;
     } else if (PS4.Triangle()) {
       v4 = 200;
     } else {
       v4 = 0;
     }
 
-    // Pneumatic control
+    // Pneumatic_A control
     if (PS4.L1()) {
-      digitalWrite(pneumatic, HIGH);
+      digitalWrite(pneumatic_A, HIGH);
     } else if (PS4.L2()) {
-      digitalWrite(pneumatic, LOW);
+      digitalWrite(pneumatic_A, LOW);
     }
 
+    // Pneumatic_B control
+    if (PS4.Up()) {
+      digitalWrite(pneumatic_B, HIGH);
+    } else if (PS4.Down()) {
+      digitalWrite(pneumatic_B, LOW);
+    }
+    
+
     // Debug info
-    Serial.printf("Vx: %.2f\tVy: %.2f\tw: %.2f   \t||\tV1: %.2f\tV2: %.2f\tV3: %.2f\tV4: %.2f\tInverse: %s\t", 
-                   Vx, Vy, w, v1, v2, v3, v4, inverse ? "ON" : "OFF");
+    Serial.printf("Vx: %.2f\tVy: %.2f\tw: %.2f   \t||\tV1: %.2f\tV2: %.2f\tV3: %.2f\tV4: %.2f\tInverse: %s\tMode: %d\n", 
+                   Vx, Vy, w, v1, v2, v3, v4, inverse ? "ON" : "OFF", mode);
     Serial.printf("Battery: %d", PS4.Battery());
     if (PS4.Charging()) Serial.print(" (Charging)");
     Serial.println();
